2903c5511b5df136ef29a65122e8c4631aa800bb
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-07-28 15:54:33 -0300

    progress with recursion

--- web/pages/index.js
+++ web/pages/index.js
@@ -19,6 +19,8 @@ const makeEmptyBoard = (width, height) => Array(width).fill(null).map(y => Array
 
 const mapBoard = (board, callback) => board.map((columnCells, y) => columnCells.map((cell, x) => callback(x, y, cell)))
 
+const cloneBoard = (board) => mapBoard(board, (x, y, value) => value)
+
 const makeMines = (width, height, mineCount) => {
   const mines = []
 
@@ -65,6 +67,33 @@ const getCellText = (board, x, y) => {
   return surroundingMineCount
 }
 
+const recursiveSolve = (board, x, y) => {
+  const newBoard = cloneBoard(board)
+
+  const width = board[0].length
+  const height = board.length
+
+  const recursive = (x, y) => {
+    newBoard[y][x] = CELL_KNOWN_CLEAR
+    if (x < width - 1 && newBoard[y][x + 1] === CELL_UNKNOWN_CLEAR && !getSurroundingMineCount(board, x + 1, y)) {
+      recursive(x + 1, y)
+    }
+    if (x > 0 && newBoard[y][x - 1] === CELL_UNKNOWN_CLEAR && !getSurroundingMineCount(board, x - 1, y)) {
+      recursive(x - 1, y)
+    }
+    if (y < height - 1 && newBoard[y + 1][x] === CELL_UNKNOWN_CLEAR && !getSurroundingMineCount(board, x, y + 1)) {
+      recursive(x, y + 1)
+    }
+    if (y > 0 && newBoard[y - 1][x] === CELL_UNKNOWN_CLEAR && !getSurroundingMineCount(board, x, y - 1)) {
+      recursive(x, y - 1)
+    }
+  }
+
+  recursive(x, y)
+
+  return newBoard
+}
+
 export default function Home() {
   const [board, setBoard] = useState(makeBoard(boardWidth, boardHeight))
   const [lostPosition, setLostPosition] = useState(null)
@@ -86,7 +115,8 @@ export default function Home() {
 
     if (value === CELL_UNKNOWN_CLEAR) {
       console.log('We are fine!')
-      setCell(x, y, CELL_KNOWN_CLEAR)
+      // setCell(x, y, CELL_KNOWN_CLEAR)
+      setBoard(recursiveSolve(board, x, y))
     } else if (value === CELL_UNKNOWN_MINE) {
       console.log('Bang!')
       setCell(x, y, CELL_KNOWN_MINE)
