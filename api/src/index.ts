fb5ee99ffcf539b166519d2a22ca72a21ddf2048
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-07-30 20:41:35 -0300

    hello world 2.0

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world')
+import cors from '@koa/cors'
+import Koa from 'koa'
+import KoaRouter from'@koa/router'
+import KoaBodyparser from 'koa-bodyparser'
+
+const router = new KoaRouter()
+
+router.get('/', (ctx, next) => {
+  ctx.status = 200
+  ctx.body = {
+    message: 'hello world',
+  }
+})
+
+const koa = new Koa()
+  .use(cors())
+  .use(KoaBodyparser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+const server = koa.listen(8000, '0.0.0.0')
