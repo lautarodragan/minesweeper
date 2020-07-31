279232ade0e776f946c32e1364fd32c3dff650c6
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-07-31 00:10:04 -0300

    [api] declare some endpoints. placeholder stuff for now

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,7 @@
 import KoaRouter from '@koa/router'
 
+const games: any[] = []
+
 export const Router = () => {
   const router = new KoaRouter()
 
@@ -8,6 +10,31 @@ export const Router = () => {
     ctx.body = {
       message: 'hello world',
     }
+  })
+
+  router.get('/games', (ctx, next) => {
+    ctx.status = 200
+    ctx.body = games
+  })
+
+  router.get('/games/:id', (ctx, next) => {
+    const { id } = ctx.params
+    const game = games.find(game => game.id === id)
+
+    if (game) {
+      ctx.status = 200
+      ctx.body = game
+    } else {
+      ctx.status = 404
+    }
+  })
+
+  router.put('/games/:id', (ctx, next) => {
+    const { id } = ctx.params
+    const game = ctx.request.body
+
+    games.push(game)
+    ctx.status = 201
   })
 
   return router
