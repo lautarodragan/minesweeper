5409cf0d676af3ad390206b156a8bdfa76c9558c
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-08-02 19:49:15 -0300

    [web] display game start date as "x units ago"

--- web/src/components/Games.jsx
+++ web/src/components/Games.jsx
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon'
 import React, { useState, useEffect } from 'react'
 
 import './Games.css'
@@ -18,10 +19,27 @@ export const Games = ({ apiClient }) => {
   )
 }
 
-const Game = ({ game }) => (
-  <li>
-    <div>Size: { game.width } x { game.height }</div>
-    <div>Mines: { game.mineCount }</div>
-    <div>Status: { game.won ? 'Won' : game.lost ? 'Lost' : 'Started' }</div>
-  </li>
-)
+const Game = ({ game }) => {
+  const time = durationAgo(game.creationDate)
+  return (
+    <li>
+      <div>Size: { game.width } x { game.height }</div>
+      <div>Mines: { game.mineCount }</div>
+      <div>Status: { game.won ? 'Won' : game.lost ? 'Lost' : 'Started' }</div>
+      <div>Started: { time }</div>
+    </li>
+  )
+}
+
+const durationAgo = (time) => {
+  const duration = DateTime.fromISO(time).diffNow().negate()
+  if (duration.as('days') > 1)
+    return Math.floor(duration.as('days')) + ' days ago'
+  if (duration.as('hours') > 1)
+    return Math.floor(duration.as('hours')) + ' hours ago'
+  if (duration.as('minutes') > 1)
+    return Math.floor(duration.as('minutes')) + ' minutes ago'
+  if (duration.as('seconds') > 1)
+    return Math.floor(duration.as('seconds')) + ' seconds ago'
+  return 'now'
+}
