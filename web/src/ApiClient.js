694b05136bb8c8c6c1bf0f1116bb46dcb46cc96a
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-08-02 07:18:56 -0300

    [web] ApiClient

---
+++ web/src/ApiClient.js
@@ -0,0 +1,58 @@
+export const ApiClient = ({ url = 'http://localhost:8000', accessToken }) => {
+  console.log('ApiClient', { url, accessToken })
+
+  const authorizationHeaders = { Authorization: `Bearer ${accessToken}` }
+  const contentTypeJsonHeaders = { 'Content-Type': 'application/json' }
+
+  async function apiFetch(resource, options) {
+    const response = await fetch(`${url}/${resource}`, {
+      ...options,
+      headers: {
+        ...authorizationHeaders,
+        ...contentTypeJsonHeaders,
+      },
+    })
+    if (response.headers.get('Content-Type').startsWith('application/json'))
+      return response.json()
+    else
+      return response.text()
+  }
+
+  const getGames = () =>  apiFetch('games')
+
+  const getGame = (gameId) => apiFetch(`games/${gameId}`)
+
+  async function createGame(game) {
+    await apiFetch(`games`, {
+      method: 'post',
+      body: JSON.stringify(game),
+    })
+  }
+
+  async function setCell(gameId, x, y, value) {
+    await apiFetch(`games/${gameId}/cells/${x},${y}`, {
+      method: 'put',
+      body: JSON.stringify({
+        value,
+      }),
+    })
+  }
+
+  async function createGameAndGet(game) {
+    await createGame(game)
+    return getGame(game.id)
+  }
+
+  async function setCellAndGet(gameId, x, y, value) {
+    await setCell(gameId, x, y, value)
+    return getGame(gameId)
+  }
+
+  return {
+    createGame,
+    createGameAndGet,
+    getGame,
+    setCell,
+    setCellAndGet,
+  }
+}
